8906336ea79f5b37ab224410133d36cb28c8a4e3
Mihir Bala <mkbala42@gmail.com>
2021-08-09 11:29:24 -0400

    Added new flightplan

--- plans/flight.py
+++ plans/flight.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         IP = "192.168.42.1"
         drone = olympe.Drone(IP)
         drone.connect()
-        drone(TakeOff()>> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
+        drone(TakeOff() >> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
         drone(moveTo(40.4154519, -79.9491731, 5.0, mode.orientation_mode.to_target, 0.0) >> moveToChanged(latitude=40.4154519, longitutde=-79.9491731, altitude=5.0, orientation_mode=mode.orientation_mode.to_target, status='DONE', _policy='wait') >> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
         drone(moveTo(40.4153334, -79.9497565, 5.0, mode.orientation_mode.to_target, 0.0) >> moveToChanged(latitude=40.4153334, longitude=-79.9497565, altitude=5.0, orientation_mode=mode.orientation_mode.to_target, status='DONE', _policy='wait') >> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
         '''
